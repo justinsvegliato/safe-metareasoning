0039390e70a746e7bdc72a076fcae09a3e1579eb
Justin Svegliato <justin.svegliato@gmail.com>
2020-08-11 17:49:17 -0400

    Made an initial architecture that has to be cleaned up

---
+++ example.py
@@ -0,0 +1,30 @@
+import mdp_solver
+import printer
+from grid_world_mdp import GridWorldMDP
+
+
+def main():
+    grid_world = [
+        ['O', 'O', 'W', 'W', 'O', 'O', 'O', 'W', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'W', 'W', 'O', 'W', 'O', 'W', 'O', 'W', 'O', 'O'],
+        ['O', 'O', 'W', 'W', 'O', 'W', 'O', 'O', 'O', 'W', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', 'W', 'W', 'W', 'W', 'W', 'O', 'O'],
+        ['O', 'O', 'W', 'W', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', 'W', 'W', 'W', 'W', 'W', 'G', 'O']
+    ]
+
+    print("Grid World Domain:")
+    printer.print_grid_world_domain(grid_world)
+
+    print("Setting up the grid world MDP...")
+    mdp = GridWorldMDP(grid_world)
+
+    print("Solving the grid world MDP...")
+    solution = cplex_mdp_solver.solve(mdp, 0.99)
+
+    print("Concrete Grid World Policy:")
+    printer.print_grid_world_policy(grid_world, solution['policy'])
+
+
+if __name__ == '__main__':
+    main()
---
+++ safety_sensitive_autonomous_system.py
@@ -0,0 +1,39 @@
+class SafetySensitiveAutonomousSystem:
+    def __init__(self, object_level_process, meta_level_controllers):
+        self.object_level_process = object_level_process
+        self.meta_level_controllers = meta_level_controllers
+
+    def resolve(self, recommendations):
+        lowest_severity_meta_level_actions = []
+        lowest_severity = float('inf')
+
+        for meta_level_action in self.meta_level_controllers[0].meta_level_actions():
+            worst_case_severity = float('-inf')
+            for recommendation in recommendations:
+                current_severity = recommendation[meta_level_action][0]
+                if current_severity > worst_case_severity:
+                    worst_case_severity = current_severity
+
+            if worst_case_severity < lowest_severity:
+                lowest_severity = worst_case_severity
+                lowest_severity_meta_level_actions = [meta_level_action]
+
+            if worst_case_severity == lowest_severity:
+                lowest_severity_meta_level_actions.append(meta_level_action)
+
+        lowest_interference = float('inf')
+        lowest_interference_meta_level_actions = []
+
+        for meta_level_action in lowest_severity_meta_level_actions:
+            worst_case_interference = float('-inf')
+            for recommendation in recommendations:
+                current_interference = recommendation[meta_level_action][1]
+                if current_interference > worst_case_interference:
+                    worst_case_interference = current_interference
+
+            if worst_case_interference < lowest_interference:
+                lowest_interference = worst_case_interference
+                lowest_interference_meta_level_actions = [meta_level_action]
+
+            if worst_case_interference == lowest_interference:
+                lowest_interference_meta_level_actions.append(meta_level_action)
