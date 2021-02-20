9bae23579aba7b4534f7c4e40988e123196904b1
Justin Svegliato <justin.svegliato@gmail.com>
2021-02-20 12:17:48 -0500

    Modified the testing file and generated a broken policy file

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ def main():
 
     safety_process = RoughTerrainSafetyProcess()
     print(json.dumps(safety_process_solver.solve(safety_process, 0.99, 0.001), indent=4))
-    # safety_process_printer.print_severity_function(safety_process)
+    # safety_process_printer.print_transition_function(safety_process)
 
 
 if __name__ == '__main__':
