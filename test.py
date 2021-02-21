a908d15b38a6dc94cd6d0239832c947fba1a72c0
Justin Svegliato <justin.svegliato@gmail.com>
2021-02-21 11:58:46 -0500

    Cleaned up this file

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import json
 
-from printers import safety_process_printer
 from safety_processes.crevice_safety_process import CreviceSafetyProcess
 from safety_processes.dust_storm_safety_process import DustStormSafetyProcess
 from safety_processes.rough_terrain_safety_process import RoughTerrainSafetyProcess
@@ -16,7 +15,6 @@ def main():
 
     safety_process = RoughTerrainSafetyProcess()
     print(json.dumps(safety_process_solver.solve(safety_process, 0.99, 0.001), indent=4))
-    # safety_process_printer.print_transition_function(safety_process)
 
 
 if __name__ == '__main__':
